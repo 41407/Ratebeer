78ed454fb3628bf2a0334727e7105e493cf49ce1
41407 <jimi.welling@cs.helsinki.fi>
2014-01-31 18:41:17 +0200

    V3 All done!

--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -6,9 +6,15 @@ class Brewery < ActiveRecord::Base
 
   validates :name, presence: true
   validates :year, numericality: {greater_than_or_equal_to: 1042,
-                                  less_than_or_equal_to: 2014,
                                   only_integer: true}
+  validate :year_must_be_less_than_current_year
 
+  def year_must_be_less_than_current_year
+    time = Time.now
+    if year > time.year
+      errors.add(:year, "can't be in the future")
+    end
+  end
 
   def print_report
     puts name
