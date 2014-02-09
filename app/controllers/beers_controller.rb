63a16ed6fff0ea1bd2c54a7d6d6ffde87d1e1177
41407 <jimi.welling@cs.helsinki.fi>
2014-02-09 13:37:24 +0200

    V4 alkuhommelit

--- app/controllers/beers_controller.rb
+++ app/controllers/beers_controller.rb
@@ -1,6 +1,8 @@
 class BeersController < ApplicationController
   before_action :set_beer, only: [:show, :edit, :update, :destroy]
 
+
+
   # GET /beers
   # GET /beers.json
   def index
@@ -10,6 +12,13 @@ class BeersController < ApplicationController
   # GET /beers/1
   # GET /beers/1.json
   def show
+  end
+
+  before_action :set_breweries_and_styles_for_template, only: [:new, :edit, :create]
+
+  def set_breweries_and_styles_for_template
+    @breweries = Brewery.all
+    @styles = ["Weizen", "Lager", "Pale ale", "IPA", "Porter"]
   end
 
   # GET /beers/new
--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,8 @@ Bundler.require(:default, Rails.env)
 module Ratebeer
   class Application < Rails::Application
     config.autoload_paths += Dir["#{Rails.root}/lib"]
+    config.i18n.enforce_available_locales = true
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
