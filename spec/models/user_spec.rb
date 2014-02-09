4d5c3ad658793b0fe17f58e81b3d72821a097455
41407 <jimi.welling@cs.helsinki.fi>
2014-02-09 13:42:07 +0200

    V4 T1

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -14,6 +14,19 @@ describe User do
     expect(User.count).to eq(0)
   end
 
+  describe "is not saved if password" do
+    it "is too short" do
+      user = User.create username:"Pekka", password:"Aa1", password_confirmation:"Aa1"
+      expect(user).not_to be_valid
+      expect(User.count).to eq(0)
+    end
+    it "consists only of letters" do
+      user = User.create username:"Pekka", password:"asdasdasd", password_confirmation:"asdasdasd"
+      expect(user).not_to be_valid
+      expect(User.count).to eq(0)
+    end
+  end
+
   describe "with a proper password" do
     let(:user){ User.create username:"Pekka", password:"Secret1", password_confirmation:"Secret1" }
 
