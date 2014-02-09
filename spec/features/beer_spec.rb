c3ef43d8795e41bb2bb8f238d7631182de40fa9e
41407 <jimi.welling@cs.helsinki.fi>
2014-02-09 17:33:53 +0200

    V4 T14

--- spec/features/beer_spec.rb
+++ spec/features/beer_spec.rb
@@ -4,11 +4,11 @@ describe "Beer" do
 
   describe "creation page" do
 
-    it "adds beer with valid name" do
-      visit new_beer_path
-      fill_in('beer_name', with: 'Koira')
+    pending "adds beer with valid name" do
+  #    visit new_beer_path
+  #    fill_in('beer_name', with: 'Koira')
 
-      click_button "Create Beer"
+  #    click_button "Create Beer"
     end
   end
 end
