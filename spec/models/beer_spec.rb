dddb5f345f529b1781986f9f8864f64ffa855053
41407 <jimi.welling@cs.helsinki.fi>
2014-02-09 13:50:44 +0200

    V4 T2

---
+++ spec/models/beer_spec.rb
@@ -0,0 +1,23 @@
+require 'spec_helper'
+
+describe Beer do
+  it "is saved if name and style are set" do
+    beer = Beer.create name:"Testiolut", style: "Lager"
+
+    expect(beer).to be_valid
+    expect(Beer.count).to eq(1)
+
+  end
+  describe "is not created if" do
+    it "name is not given" do
+      beer = Beer.create style: "Lager"
+
+      expect(beer).not_to be_valid
+    end
+    it "style is not given" do
+      beer = Beer.create name: "Testiolut"
+
+      expect(beer).not_to be_valid
+    end
+  end
+end
